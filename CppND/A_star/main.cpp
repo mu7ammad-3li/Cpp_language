5da53a329338374e54f4b84865b1266301c1e3c0
mu7ammad-3li <mu7ammad.3lii@gmail.com>
2020-04-06 09:44:34 +0200

    Read from file

--- CppND/A_star/main.cpp
+++ CppND/A_star/main.cpp
@@ -1,29 +1,31 @@
 #include<iostream> 
 #include <vector> 
+#include <string>
+#include <fstream>
 
 using namespace std ; 
 
-void printBoard(vector<vector<int>> aBoard); 
+void printBoard(string fPath ); 
 
 int main() 
 {
-    vector<vector<int>> board {{0, 1, 0, 0, 0, 0},
-                                {0, 1, 0, 0, 0, 0},
-                                {0, 1, 0, 0, 0, 0},
-                                {0, 1, 0, 0, 0, 0},
-                                {0, 0, 0, 0, 1, 0}}; 
-    
-    printBoard(board);
+    printBoard("Files/1.board");
     return 0; 
-
 }
 
-void printBoard(vector<vector<int>> aBoard)
+void printBoard(string fPath )
 {
-    for ( const auto &row : aBoard )
+    ifstream board_file;
+    board_file.open(fPath);
+
+    if (board_file)
     {
-            for ( const auto &s : row )
-            cout <<  s << ' ';
-            cout << endl;
-    }        
+        cout <<"File stream Created "<<endl; 
+        string line; 
+        while (getline(board_file,line))
+        {
+            cout <<line<<endl; 
+        }
+        
+    }     
 }
