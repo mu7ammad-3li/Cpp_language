aa709efd33cddee815b12ef15a299f9e58130d9e
mu7ammad-3li <mu7ammad.3lii@gmail.com>
2020-04-17 18:20:34 +0200

    gathering all files into main

--- CppND/A_star/main.cpp
+++ CppND/A_star/main.cpp
@@ -1,31 +1,79 @@
-#include<iostream> 
-#include <vector> 
-#include <string>
+#include <iostream>
+#include <vector>
 #include <fstream>
+#include <string>
+#include <sstream>
 
-using namespace std ; 
+using namespace std;
 
-void printBoard(string fPath ); 
+enum class state{kEmpty,kObstacle};
 
-int main() 
+vector<state> ParseLine(string Line);
+vector<vector<state>> ReadBoardFile(string path);
+void PrintBoard(const vector<vector<state>> board);
+
+int main()
 {
-    printBoard("Files/1.board");
-    return 0; 
+  auto board = ReadBoardFile("1.board");
+  PrintBoard(board);
 }
 
-void printBoard(string fPath )
+string CellString(state cell)
 {
-    ifstream board_file;
-    board_file.open(fPath);
-
-    if (board_file)
+    switch (cell)
     {
-        cout <<"File stream Created "<<endl; 
-        string line; 
-        while (getline(board_file,line))
+    case state::kObstacle:
+        return "🚧   ";
+    default:
+        return "🚦   ";
+    }
+}
+vector<state> ParseLine(string Line)
+{
+    vector<state> Row;
+
+    istringstream Fstream(Line); //intialize a string stream
+    int n;
+    char c;
+    while (Fstream >> n >> c)
+    {
+        if (n==0)
+            Row.push_back(state::kEmpty);
+        else
+            Row.push_back(state::kObstacle);
+    }
+    return Row; 
+}
+
+vector<vector<state>> ReadBoardFile(string path)
+{
+    ifstream boardFile;
+    vector<vector<state>> boardVector;
+    boardFile.open("Files/1.board");
+
+    if (boardFile)
+    {
+        cout << "File Stream is sucsessful" << endl;
+        string Line;
+        while (getline(boardFile, Line))
         {
-            cout <<line<<endl; 
+            vector<state> Row;
+            Row = ParseLine(Line);
+            boardVector.push_back(Row);
         }
-        
-    }     
+        return boardVector;
+    }
+    else
+    {
+        cerr << "File stream failed " << endl;
+        return boardVector;
+    }
+}
+void PrintBoard(const vector<vector<state>> board) {
+  for (int i = 0; i < board.size(); i++) {
+    for (int j = 0; j < board[i].size(); j++) {
+      cout << CellString(board[i][j]);
+    }
+    cout << "\n";
+  }
 }
